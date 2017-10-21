9fd76d98dea6acf680bb393042dd16dd7e35a01f
Luis <luisalberto_zelda@hotmail.com>
2017-10-21 12:54:07 -0500

    Primera Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  nombre = 'Luis';
+  nombre2 = 'Ganaste un Ipad, clic aqui para reclamar.';
+  
+  num1 = 324234;
+  num2 = 234;
+  num0 = this.num1+this.num2;
+  
 }
