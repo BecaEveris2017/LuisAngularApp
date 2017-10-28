dbc7970bd09967cee85636636c46d7cb1d7d07a4
Luis <luisalberto_zelda@hotmail.com>
2017-10-28 09:38:19 -0500

    Tarea 1 y 2

--- src/app/little-tour.component.ts
+++ src/app/little-tour.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'little-tour',
+  selector: 'listaColores',
   template: `
-    <input #newcolores
+    
+  
+  <input #newcolores
+
       (keyup.enter)="addcolores(newcolores.value)"
+
       (blur)="addcolores(newcolores.value); newcolores.value='' ">
       
-      <ul><li *ngFor="let colores of colores">{{colores}}</li></ul>
-      <ul><li [hidden]="!condicion">{{colores}}</li></ul>
-      
+      <ul><li  *ngFor="let colores of colores" [hidden]="!condicion">{{colores}}</li></ul>
 
     <button (click)="addcolores(newcolores.value)">Agregar</button>
-    <button (click)="addcolores(newcolores.value)">Mostrar/Ocultar</button>
+    <button (click)="mostrarColores(condicion)">Mostrar/Ocultar</button>
 
-    
-  `
+    `
 })
 export class LittleTourComponent {
   colores = ['Rojo', 'Verde', 'Negro'];
   condicion = true;
+
+  mostrarColores (){
+this.condicion=!this.condicion;
+  }
+
   addcolores(newcolores: string) {
     if (newcolores) {
       this.colores.push(newcolores);
